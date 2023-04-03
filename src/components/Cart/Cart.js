4268151ff99c0cd6df9f0d803eb03e3b3b56ecec
ogedengbewisdom <ogedengbewisdom1994@gmail.com>
2023-04-03 11:48:26 +0100

    using the map method to render the cartitem component instead of hardcoding

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,15 +1,22 @@
+import { useSelector } from 'react-redux';
 import Card from '../UI/Card';
 import classes from './Cart.module.css';
 import CartItem from './CartItem';
 
+
 const Cart = (props) => {
+
+  const item = useSelector(state => state.cartItem)
+  // const item = [{ title: 'Test Item', quantity: 3, total: 18, price: 6 }]
+  
   return (
     <Card className={classes.cart}>
       <h2>Your Shopping Cart</h2>
       <ul>
-        <CartItem
-          item={{ title: 'Test Item', quantity: 3, total: 18, price: 6 }}
-        />
+        {/* <CartItem item={dummy}/> */}
+        {item.map(cart => 
+          <CartItem key={Math.random()} title={cart.title} quantity={cart.quantity} total={cart.total} price={cart.price} />
+          )}
       </ul>
     </Card>
   );
