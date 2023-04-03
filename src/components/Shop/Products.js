41af3ff191c1d4209367654bc2168cb78f9ac11f
ogedengbewisdom <ogedengbewisdom1994@gmail.com>
2023-04-03 13:39:14 +0100

    use redux actions and dispatch

--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -1,19 +1,25 @@
 import ProductItem from './ProductItem';
 import classes from './Products.module.css';
+import { useDispatch } from 'react-redux';
+import { cartSliceActions } from '../../store/cart';
 
 const Products = (props) => {
+
+  const dispatch = useDispatch()
+  const addToCartHandler = (data) => {
+    dispatch(cartSliceActions.addToCart(data))
+  }
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
+        <ProductItem onAddCart={addToCartHandler}
+          // title='Test'
+          // price={6}
+          // description='This is a first product - amazing!'
         />
       </ul>
     </section>
   );
 };
-
 export default Products;
