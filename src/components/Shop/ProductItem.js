b78470ebd44243547143b94c3f05ec54b558872a
ogedengbewisdom <ogedengbewisdom1994@gmail.com>
2023-04-03 13:37:40 +0100

    use react hook

--- src/components/Shop/ProductItem.js
+++ src/components/Shop/ProductItem.js
@@ -1,27 +1,60 @@
-import { useDispatch } from 'react-redux';
+
 import Card from '../UI/Card';
 import classes from './ProductItem.module.css';
-import { cartSliceActions } from '../../store/cart';
+
+import useInput from '../../hook/useInput';
 const ProductItem = (props) => {
-  const { title, price, description } = props;
+  // const { title, price, description } = props;
 
-  const dispatch = useDispatch()
+  const {value: title, valueChangeHandler: titleChangeHandler, reset: resetTitle} = useInput(value => value.trim() !== "")
 
-  const addToCartHandler = () => {
-    dispatch(cartSliceActions.addToCart({ title: 'Test Item', quantity: 3, total: 18, price: 6 }))
+  const {value: quantity, valueChangeHandler: quantityChangeHandler, reset: resetQuantity} = useInput(value => value > 0)
+
+  const {value: price, valueChangeHandler: priceChangeHandler, reset: resetPrice} = useInput(value => value > 0)
+
+  
+
+
+
+  const submitHandler = (event) => {
+    event.preventDefault()
+    const data = {
+      title: title,
+      quantity: +quantity,
+      price: +price,
+      total: quantity * price
+    }
+
+    props.onAddCart(data)
+    resetPrice()
+    resetQuantity()
+    resetTitle()
   }
 
   return (
     <li className={classes.item}>
       <Card>
-        <header>
+
+        <form onSubmit={submitHandler}>
+          <label>Title</label>
+          <input type='text' onChange={titleChangeHandler} value={title}></input>
+
+          <label>Quantity</label>
+          <input type='number' onChange={quantityChangeHandler} value={quantity}></input>
+
+          <label>Price</label>
+          <input type='number' onChange={priceChangeHandler} value={price}></input>
+
+          <button>Add to cart</button>
+        </form>
+        {/* <header>
           <h3>{title}</h3>
           <div className={classes.price}>${price.toFixed(2)}</div>
         </header>
         <p>{description}</p>
         <div className={classes.actions}>
           <button  onClick={addToCartHandler}>Add to Cart</button>
-        </div>
+        </div> */}
       </Card>
     </li>
   );
