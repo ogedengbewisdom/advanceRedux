6fe59473e71c2c284e01cc58b2313f9383339919
ogedengbewisdom <ogedengbewisdom1994@gmail.com>
2023-04-03 11:46:57 +0100

    making use of the redux

--- src/components/Cart/CartButton.js
+++ src/components/Cart/CartButton.js
@@ -1,10 +1,22 @@
+import { useDispatch, useSelector } from 'react-redux';
 import classes from './CartButton.module.css';
+import { cartSliceActions } from '../../store/cart';
 
 const CartButton = (props) => {
+  const item = useSelector(state => state.cartItem)
+  const cartItems = item.reduce((index, item) => {
+    return index + item.quantity
+  }, 0)
+
+  const dispatch = useDispatch()
+
+  const showCartHandler = () => {
+    dispatch(cartSliceActions.show())
+  }
   return (
-    <button className={classes.button}>
+    <button type='button' onClick={showCartHandler} className={classes.button}>
       <span>My Cart</span>
-      <span className={classes.badge}>1</span>
+      <span className={classes.badge}>{cartItems}</span>
     </button>
   );
 };
